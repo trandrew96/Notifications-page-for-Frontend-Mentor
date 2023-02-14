072b96a2a3271df324cac2e46476b12cd7ae1392
trandrew <19401994+trandrew96@users.noreply.github.com>
2023-02-14 10:30:38 -0800

    first commit of actual custom code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Notification from "./Notification";
+import Notifications from "./api-response.json";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(3);
+
+  const notifications = Notifications["notifications"].map((data) => (
+    <Notification
+      avatar={process.env.PUBLIC_URL + data.avatar}
+      name={data.name}
+      action={data.action}
+      post={data.reference_name}
+      time={data.time}
+      message={data.message}
+      picture={data.picture}
+      unread={count === 0 ? "" : data.unread}
+    ></Notification>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto px-3 md:px-2 max-w-2xl">
+      {/* navbar */}
+      <div className="flex items-center my-8">
+        <div className="font-extrabold text-lg">Notifications</div>
+        <div className="font-extrabold text-white bg-blue-900 px-3 rounded ml-2 text-lg">
+          {count}
+        </div>
+        <div className="ml-auto font-medium text-gray-500">
+          <button onClick={() => setCount(0)}>Mark all as read</button>
+        </div>
+      </div>
+      <div>{notifications}</div>
     </div>
   );
 }
---
+++ src/Notification.js
@@ -0,0 +1,116 @@
+import "./Notification.css";
+
+function Notification(props) {
+  // Render the content of the notification depending on its type
+  let action;
+  switch (props.action) {
+    case "reacted":
+      action = (
+        <span>
+          <span className="ml-2">reacted to your recent post</span>
+          <a
+            href="/"
+            className="font-bold text-gray-500 hover:text-blue-900 ml-2"
+          >
+            {props.post}
+          </a>
+        </span>
+      );
+      break;
+    case "followed":
+      action = <span className="ml-2">followed you</span>;
+      break;
+    case "joined":
+      action = (
+        <span>
+          <span className="ml-2">has joined your group</span>{" "}
+          <a
+            href="/"
+            className="font-bold text-gray-500 hover:text-blue-900 ml-2"
+          >
+            {props.post}
+          </a>
+        </span>
+      );
+      break;
+    case "message":
+      action = <span className="ml-2">sent you a private message</span>;
+      break;
+    case "commented":
+      action = <span className="ml-2">commented on your picture</span>;
+      break;
+    case "left":
+      action = (
+        <span>
+          <span className="ml-2">left the group</span>{" "}
+          <a
+            href="/"
+            className="font-bold text-gray-500 hover:text-blue-900 ml-2"
+          >
+            {props.post}
+          </a>
+        </span>
+      );
+      break;
+  }
+
+  // Render private message if applicable
+  let message;
+  if (props.message) {
+    message = (
+      <a href="/">
+        <div className="border-2 md:border-0 p-3 mt-2 hover:bg-custom-blue rounded">
+          <span>{props.message}</span>
+        </div>
+      </a>
+    );
+  }
+
+  // Render posted picture if applicable
+  let picture;
+  if (props.action === "commented") {
+    picture = (
+      <div className="ml-auto">
+        <a href="/">
+          <img
+            className="w-10 max-w-sm"
+            src={props.picture}
+            alt={"thumbnail for " + props.name + "'s picture"}
+          />
+        </a>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="flex flex-row text-gray-500 my-10">
+        <div className="px-5">
+          <img
+            className="w-10 max-w-sm"
+            src={props.avatar}
+            alt={"profile picture for " + props.name}
+          />
+        </div>
+        <div className="">
+          <span>
+            <a href="/" className="font-bold text-black hover:text-blue-900">
+              {props.name}
+            </a>
+            {action}
+            {props.unread && (
+              <div className="w-3 h-3 bg-red-400 rounded-full ml-2 inline-block"></div>
+            )}
+          </span>
+
+          <p className="text-gray-400">{props.time}</p>
+
+          {message}
+        </div>
+        {picture}
+      </div>
+    </div>
+  );
+}
+
+export default Notification;
